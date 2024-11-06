2910d2c62ba8cfca520fc79de6beaedfe8b2883e
tracecaDev <traseca.news@mail.ru>
2024-11-06 18:08:33 +0500

    add csp header

--- middleware.js
+++ middleware.js
@@ -1,8 +1,15 @@
 import { withAuth } from "next-auth/middleware";
 import { NextResponse } from "next/server";
 
+const csp = "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval'; style-src 'self' 'unsafe-inline'; img-src 'self' data:; font-src 'self'; connect-src 'self'; frame-src 'none';";
+
+
 export default withAuth(
   function middleware(req) {
+    // Create response with CSP header
+    const response = NextResponse.next();
+    response.headers.set("Content-Security-Policy", csp);
+
     console.log(req.nextUrl.pathname);
     console.log(req.nextauth.token.role);
 
@@ -12,6 +19,8 @@ export default withAuth(
     ) {
       return NextResponse.rewrite(new URL("/Denied", req.url));
     }
+    
+    return response;
   },
   {
     callbacks: {
